5e4615ccf3248d161a5c1737093585aebc6a6c55
c7wx <adriangrahaminbox2@tutanota.com>
2023-10-23 19:21:58 +0100

    Starting work on load menu
    Starting framework for world load menu.

---
+++ src/states/LoadGameMenuState.py
@@ -0,0 +1,19 @@
+from src.states.StateBase import StateBase
+from src.gui.TextButton import TextButton
+from src.gui.TextLabel import TextLabel
+
+class LoadGameMenuState(StateBase):
+    def __init__(self, game):
+        super().__init__(game)
+        self._gui = {
+
+        }
+
+    def on_state_enter(self):
+        pass
+
+    def on_state_leave(self):
+        pass
+
+    def loop(self):
+        pass
